9824f6cb74f3d02e0b4eb18f7f4e9b3f0ad3cc55
rcjones <rcjones.iphone@gmail.com>
2015-06-08 20:28:44 -0500

    Initial Commit

---
+++ StatesAndCapsTests/StatesAndCapsTests.swift
@@ -0,0 +1,36 @@
+//
+//  StatesAndCapsTests.swift
+//  StatesAndCapsTests
+//
+//  Created by Ryan Jones on 6/8/15.
+//  Copyright (c) 2015 Ryan Jones. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class StatesAndCapsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
