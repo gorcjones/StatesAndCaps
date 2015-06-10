17d337b21634f612bb73bbcfab99aaaf30e3c5fa
rcjones <rcjones.iphone@gmail.com>
2015-06-10 04:50:02 -0500

    StatsNCaps struct

---
+++ StatesAndCaps/StatesNCaps.swift
@@ -0,0 +1,15 @@
+//
+//  StatesNCaps.swift
+//  StatesAndCaps
+//
+//  Created by Ryan Jones on 6/10/15.
+//  Copyright (c) 2015 Ryan Jones. All rights reserved.
+//
+
+import Foundation
+
+struct StatsNCaps {
+    var state = "Alaska"
+    var capital = "Juneau"
+    var isUsed = false
+}
